b4a2544f4e05b7699ba6a216ad25cff9562dca65
tbrou <tbrou@tbrou-PC7.mshome.net>
2017-12-05 20:13:08 +0100

    * prepared the test for update

--- iam-core/test/fr/epita/iam/services/test/TestFileOperations.java
+++ iam-core/test/fr/epita/iam/services/test/TestFileOperations.java
@@ -31,6 +31,32 @@ public class TestFileOperations {
 
 	public static void main(String[] args) throws IOException {
 
+		testCreateAndSearch();
+
+	}
+
+	/**
+	 * <h3>Description</h3>
+	 * <p>This methods allows to ...</p>
+	 *
+	 * <h3>Usage</h3>
+	 * <p>It should be used as follows :
+	 *
+	 * <pre><code> ${enclosing_type} sample;
+	 *
+	 * //...
+	 *
+	 * sample.${enclosing_method}();
+	 *</code></pre>
+	 * </p>
+	 *
+	 * @since $${version}
+	 * @see Voir aussi $${link}
+	 * @author ${user}
+	 *
+	 * ${tags}
+	 */
+	private static void testCreateAndSearch() throws IOException {
 		// Given : initial context
 		final FileIdentityDAO dao = new FileIdentityDAO("/tmp/2017s2_b/identities.txt");
 
@@ -42,6 +68,55 @@ public class TestFileOperations {
 		// When : we execute the action
 		dao.create(id1);
 
+
+		// Then : we expect the following result to verify the test.
+		final Identity criteria = new Identity();
+		criteria.setDisplayName("Thom");
+		final List<Identity> identities = dao.search(criteria);
+		if (identities.get(0).equals(id1)) {
+			System.out.println("success");
+		} else {
+			System.out.println("failure");
+		}
+	}
+
+	/**
+	 * <h3>Description</h3>
+	 * <p>
+	 * This methods allows to ...
+	 * </p>
+	 *
+	 * <h3>Usage</h3>
+	 * <p>
+	 * It should be used as follows :
+	 * 
+	 * <pre>
+	 * <code> ${enclosing_type} sample;
+	 *
+	 * //...
+	 *
+	 * sample.${enclosing_method}();
+	 *</code>
+	 * </pre>
+	 * </p>
+	 * 
+	 * @since $${version}
+	 * @see Voir aussi $${link}
+	 * @author ${user}
+	 *
+	 *         ${tags}
+	 */
+	private static void testCreateUpdateAndSearch() throws IOException {
+		// Given : initial context
+		final FileIdentityDAO dao = new FileIdentityDAO("/tmp/2017s2_b/identities.txt");
+
+		final Identity id1 = new Identity();
+		id1.setDisplayName("Thomas");
+		id1.setUid("123");
+		id1.setEmail("tbr@tbr.com");
+
+		// When : we execute the action
+		dao.create(id1);
 
 		// Then : we expect the following result to verify the test.
 		final Identity criteria = new Identity();
